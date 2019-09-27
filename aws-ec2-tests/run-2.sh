20c1103e506738bf05405a9e614c3270030a54a7
mliszcz <liszcz.michal@gmail.com>
2019-09-27 18:02:16 +0000

    client 9xl results

--- aws-ec2-tests/run-2.sh
+++ aws-ec2-tests/run-2.sh
@@ -58,14 +58,48 @@ device_to_server=(
 
 
 # server testing
-for inst_size in large xlarge 2xlarge 4xlarge 9xlarge 18xlarge; do
+# for inst_size in large xlarge 2xlarge 4xlarge 9xlarge 18xlarge; do
+#   for bmark in read write; do
+#     for device in $TGT_PY $TGT_CPP $TGT_JAVA; do
+#       cpp_client=${cpp_client_name[$bmark]}
+#       java_client=${java_client_name[$bmark]}
+#       server=${device_to_server[$device]}
+#       instance="${size_to_hostname_tgt[$inst_size]}"
+#       for client_kind in py; do
+# 
+#         declare -A client_kind_to_opts
+#         client_kind_to_opts=(
+#           ['py']=''
+#           ['cpp']="--worker tangobenchmarks.client.external.Worker --worker-program tg_benchmark_client_${cpp_client}"
+#           ['java']="--worker tangobenchmarks.client.external.Worker --worker-program tg_benchmark_client_java_${java_client}"
+#         )
+# 
+#         client_opts="${client_kind_to_opts[$client_kind]}"
+# 
+#         echo "Running $client_kind $bmark ($cpp_client) on $device ... @ $server @ $instance with: ${client_opts}"
+# 
+#         ssh -i ~/aws-keypar-01.pem ubuntu@$instance "TANGO_HOST=$TANGO_HOST TANGO_ROOT=/usr/local $server 01" &>/dev/null &
+#         sleep 2
+# 
+#         run_benchmark $client_kind $bmark $device $inst_size ${client_opts}
+# 
+#         ssh -i ~/aws-keypar-01.pem ubuntu@$instance killall PyBenchmarkTarget CppBenchmarkTarget JavaBenchmarkTarget java &>/dev/null || true
+# 
+#       done
+#     done
+#   done
+# done
+
+
+# client testing
+for inst_size in 18xlarge; do
   for bmark in read write; do
-    for device in $TGT_PY $TGT_CPP $TGT_JAVA; do
+    for device in $TGT_PY; do
       cpp_client=${cpp_client_name[$bmark]}
       java_client=${java_client_name[$bmark]}
       server=${device_to_server[$device]}
       instance="${size_to_hostname_tgt[$inst_size]}"
-      for client_kind in py; do
+      for client_kind in py cpp java; do
 
         declare -A client_kind_to_opts
         client_kind_to_opts=(
@@ -89,37 +123,3 @@ for inst_size in large xlarge 2xlarge 4xlarge 9xlarge 18xlarge; do
     done
   done
 done
-
-
-# client testing
-# for inst_size in 18xlarge; do
-#   for bmark in read write; do
-#     for device in $TGT_PY; do
-#       cpp_client=${cpp_client_name[$bmark]}
-#       java_client=${java_client_name[$bmark]}
-#       server=${device_to_server[$device]}
-#       instance="${size_to_hostname_tgt[$inst_size]}"
-#       for client_kind in py cpp java; do
-
-#         declare -A client_kind_to_opts
-#         client_kind_to_opts=(
-#           ['py']=''
-#           ['cpp']="--worker tangobenchmarks.client.external.Worker --worker-program tg_benchmark_client_${cpp_client}"
-#           ['java']="--worker tangobenchmarks.client.external.Worker --worker-program tg_benchmark_client_java_${java_client}"
-#         )
-
-#         client_opts="${client_kind_to_opts[$client_kind]}"
-
-#         echo "Running $client_kind $bmark ($cpp_client) on $device ... @ $server @ $instance with: ${client_opts}"
-
-#         ssh -i ~/aws-keypar-01.pem ubuntu@$instance "TANGO_HOST=$TANGO_HOST TANGO_ROOT=/usr/local $server 01" &>/dev/null &
-#         sleep 2
-
-#         run_benchmark $client_kind $bmark $device $inst_size ${client_opts}
-
-#         ssh -i ~/aws-keypar-01.pem ubuntu@$instance killall PyBenchmarkTarget CppBenchmarkTarget JavaBenchmarkTarget java &>/dev/null || true
-
-#       done
-#     done
-#   done
-# done
